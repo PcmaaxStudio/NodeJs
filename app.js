7f48b5bb0353f11537a3ae440172919d00d958a0
Amal Mulia <amalmulia.pcmaax@yahoo.com>
2017-04-26 13:18:52 +0800

    Koneksi API sudah bisa

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
-// Problem : kita kan menampilkan profile github sesorang
-// solusi : menggunakan NoddeJs untuk connect ke GitHub untuk mendapatkan info dan print ke console
 
-// TODO: Connect to GitHub API
 let https = require('https')
 
-https.get('https://api.github.com/user/yofri', (result) => {
-	console.log('got responses: ', result.statusCode)
+const options = {
+	hostname: 'api.github.com',
+	port: 443,
+	path: '/users/pcmaaxstudio',
+	method: 'GET',
+	headers: {
+		'user-agent': 'nodejs'
+	}
+}
+
+let request = https.request(options, (result) => {
+	console.log('Got response: ', result.statusCode)
 })
-// TODO: Baca Data
-// TODO: Parsing Data
-// TODO: Print ke console
 
+request.end()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let menampilkan = function(param) {
-// 	console.log('ini fungsi menampilkan ' + param)
-// }
-
-// let menampilkan2 = (param) => {
-// 	console.log('ini fungsi menampilkan ' + param)
-// }
-// menampilkan('data')
-// menampilkan2('data2')
-
-
+request.on('error', (e) => {
+	console.error(e)
+})
